9183df10007378ba9c3ea1570f105e82bb3cd627
Jinwoo Choi <cgzoo9607@gmail.com>
2022-06-08 20:46:21 +0900

    20220607 완료, 리트라이 해볼 예정

--- src/components/20220607/CreateUser.jsx
+++ src/components/20220607/CreateUser.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-const CreateUser = ({onCreate}) => {
+const CreateUser = ({ nickname, email, onChange, onCreate, nicknameRef, emailRef }) => {
+  
   return (
-    <div>
-      <input type="text" />
-      <input type="text" />
-      <button>등록</button>
+    <div className='createUser'>
+      <input
+        type="text"
+        name="nickname"
+        placeholder="이름"
+        value={nickname}
+        onChange={onChange}
+        // ref={nicknameRef}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="이메일"
+        value={email}
+        onChange={onChange}
+        // ref={emailRef}
+        onKeyUp={e => { if (e.key === 'Enter') onCreate() }}
+      />
+      <button onClick={onCreate}>등록</button>
     </div>
   );
 };
 
-export default CreateUser;
+export default React.memo(CreateUser);
--- src/components/20220607/UserList.jsx
+++ src/components/20220607/UserList.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import CreateUser from './CreateUser';
+import styles from './UserList.module.css';
 
-const UserList = () => {
+const User = React.memo(({ user,onRemove,onToggle }) => {
+	const { nickname, email, id, active } = user;
 	return (
-		<>
-      <CreateUser />
-		</>
+		<div>
+			<b className={active ? `${styles.active} ${styles.content}` : `${styles.content}`} onClick={() => onToggle(id)}>{nickname}</b>
+			&nbsp;
+			({email})
+			<button onClick={()=>{onRemove(id)}}>삭제</button>
+		</div>
+	)
+})
+
+const UserList = ({ users, onRemove, onToggle }) => {
+	return (
+		<div className='userList'>
+			{
+				users.map((user) =>
+					<User
+						user={user}
+						key={user.id}
+						onRemove={onRemove}
+						onToggle={onToggle}
+					/>
+				)
+			}
+		</div>
 	);
 };
 
-export default UserList;
+export default React.memo(
+	UserList,
+	(prevProps, nextProps) => prevProps.users === nextProps.users
+	);
